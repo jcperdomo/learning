030a88f10353ec18a9656ffab778d0c4f8404038
mdgoldberg <mgoldberg@college.harvard.edu>
2016-04-29 20:55:10 -0400

    initial interface

--- interface.py
+++ interface.py
@@ -1,2 +1,24 @@
-TODO
 
+
+class Game:
+    def __init__(self, filename):
+        pass
+
+    def utility(self, actions):
+        """Returns utilities given an action profile.
+
+        :actions: List of actions with length equal to number of players
+        :returns: TODO
+
+        """
+        pass
+
+
+def sampleMixedStrat(mixed):
+    """Samples a pure strategy from a mixed strategy.
+
+    :mixed: list of tuples (a, p) where the player plays action a with
+    probability p. Probabilities must sum to 1.
+    :returns: One of the actions a sampled from the mixed strategy argument.
+    """
+    pass
